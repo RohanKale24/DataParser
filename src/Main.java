6f9d11d02b910a209b41989dbc282e27d4505069
Rohan Kale <rkale843@student.fuhsd.org>
2019-03-01 16:53:44 -0800

    Tested Utils Class

--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,9 @@
  */
 
 public class Main {
+    public static void main(String[] args) {
+        Utils u = new Utils();
+        System.out.println(u.readFileAsString("C:\\Users\\gamem\\IdeaProjects\\DataParser\\data\\2016_Presidential_Results.csv"));
+    }
+
 }
