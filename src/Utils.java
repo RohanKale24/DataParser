be912265b4a2775067f9a58a06a16f7d5c34c138
Rohan Kale <rkale843@student.fuhsd.org>
2019-03-03 15:30:24 -0800

    formatting

--- src/Utils.java
+++ src/Utils.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Utils {
 
-    public Utils(){
+    public Utils() {
 
     }
 
@@ -25,7 +25,7 @@ public class Utils {
         return output.toString();
     }
 
-    public ArrayList<ElectionResult> parseElectionResults(String data){
+    public ArrayList<ElectionResult> parseElectionResults(String data) {
         ArrayList<ElectionResult> results = new ArrayList<>();
         String[] lines = data.split("\n");
 
@@ -33,37 +33,24 @@ public class Utils {
             String line = lines[i];
 
 
-
-
-
-
-
-
-            if(line.contains("\"")) {
+            if (line.contains("\"")) {
                 int indexOfApostrophe = line.indexOf("\"");
-                int indexOfComma = line.indexOf(",",indexOfApostrophe);
+                int indexOfComma = line.indexOf(",", indexOfApostrophe);
 
 
+                String dataWithoutPercentage = line.substring(0, line.indexOf("%")) +
+                        line.substring(line.indexOf("%") + 1);
 
-                String dataWithoutPercentage = line.substring(0,line.indexOf("%"))+
-                        line.substring(line.indexOf("%")+1);
+                String dataWithoutExtraComma = dataWithoutPercentage.substring(0, indexOfComma) + dataWithoutPercentage.substring(indexOfComma + 1);
 
-                String dataWithoutExtraComma = dataWithoutPercentage.substring(0,indexOfComma) + dataWithoutPercentage.substring(indexOfComma+1);
-
-                if(dataWithoutExtraComma.indexOf(",",indexOfApostrophe)<dataWithoutExtraComma.indexOf("\"",indexOfApostrophe+1)&&dataWithoutExtraComma.indexOf(",",indexOfApostrophe)!=-1){
+                if (dataWithoutExtraComma.indexOf(",", indexOfApostrophe) < dataWithoutExtraComma.indexOf("\"", indexOfApostrophe + 1) && dataWithoutExtraComma.indexOf(",", indexOfApostrophe) != -1) {
 
 
-                    int newIndexOfComma = dataWithoutExtraComma.indexOf(",",indexOfApostrophe);
-                     dataWithoutExtraComma = dataWithoutExtraComma.substring(0,newIndexOfComma) + dataWithoutExtraComma.substring(newIndexOfComma+1);
+                    int newIndexOfComma = dataWithoutExtraComma.indexOf(",", indexOfApostrophe);
+                    dataWithoutExtraComma = dataWithoutExtraComma.substring(0, newIndexOfComma) + dataWithoutExtraComma.substring(newIndexOfComma + 1);
 
 
                 }
-
-
-
-
-
-
 
 
                 String dataWithoutFirstApostrophe = dataWithoutExtraComma.substring(0, indexOfApostrophe) +
@@ -71,19 +58,6 @@ public class Utils {
                 int indexOfSecondApostrophe = dataWithoutFirstApostrophe.indexOf("\"");
                 String dataWithoutSecondApostrophe = dataWithoutFirstApostrophe.substring(0, indexOfSecondApostrophe)
                         + dataWithoutFirstApostrophe.substring(indexOfSecondApostrophe + 1);
-
-
-                
-
-
-
-
-
-
-
-
-
-
 
 
                 String[] categories = dataWithoutSecondApostrophe.split(",");
@@ -100,19 +74,11 @@ public class Utils {
                 ElectionResult electionResult = new ElectionResult(demVotes, gopVotes, totalVotes, votesPerDem, votesPerGop, diffBtwnDemAndGop, perPointDiff, state, county, combinedFips);
                 electionResult.toString();
                 results.add(electionResult);
-            }
+            } else {
 
-
-
-
-
-
-
-            else{
-
-                String dataWithoutPercentage = line.substring(0,line.indexOf("%"))+
-                        line.substring(line.indexOf("%")+1);
-                String [] categories = dataWithoutPercentage.split(",");
+                String dataWithoutPercentage = line.substring(0, line.indexOf("%")) +
+                        line.substring(line.indexOf("%") + 1);
+                String[] categories = dataWithoutPercentage.split(",");
                 int demVotes = (int) Double.parseDouble(categories[1]);
                 int gopVotes = (int) Double.parseDouble(categories[2]);
                 int totalVotes = (int) Double.parseDouble(categories[3]);
@@ -130,7 +96,6 @@ public class Utils {
         }
         return results;
     }
-
 
 
 }
