eb58ce1744460553deea87a907d61dcf5ece8ffc
Rohan Kale <rkale843@student.fuhsd.org>
2019-03-10 21:14:12 -0700

    added more getter methods for finding counties and other helper methods

--- src/State.java
+++ src/State.java
@@ -4,7 +4,7 @@ public class State {
     private String name;
     private ArrayList<County> counties;
 
-    public State(String name){
+    public State(String name) {
         this.name = name;
         counties = new ArrayList<>();
     }
@@ -24,12 +24,41 @@ public class State {
     public void setCounties(ArrayList<County> counties) {
         this.counties = counties;
     }
-    public void setCountie(County newCounty){
-        for(int i = 0; i < counties.size(); i++){
+
+    public void setCountie(County newCounty) {
+        for (int i = 0; i < counties.size(); i++) {
             County county = counties.get(i);
             if (county.getFips() == newCounty.getFips()) county = newCounty;
         }
 
+    }
+
+    public int countyIndex(String name) {
+        for (int i = 0; i < counties.size(); i++) {
+            County county = counties.get(i);
+            if (county.getName().equals(name)) return i;
+        }
+        return -1;
+    }
+
+    public int countyIndex(int fips) {
+        for (int i = 0; i < counties.size(); i++) {
+            County county = counties.get(i);
+            if (county.getFips() == fips) return i;
+        }
+        return -1;
+    }
+
+    public void add(County county) {
+        counties.add(county);
+    }
+
+    public void removeCounty(County county) {
+        counties.remove(county);
+    }
+
+    public County removeCounty(int index) {
+        return counties.remove(index);
     }
 
 
