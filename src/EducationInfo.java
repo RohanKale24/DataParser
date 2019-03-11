5be8a2d95764bb46852efc76fc4c0f2c130a87be
Rohan Kale <rkale843@student.fuhsd.org>
2019-03-10 18:35:55 -0700

    Made getter methods for EducationInfo

--- src/EducationInfo.java
+++ src/EducationInfo.java
@@ -1,10 +1,28 @@
 public class EducationInfo {
-    private double noHighSchool,onlyHighSchool,someCollege,bachelorsOrMore;
+    private double noHighSchool, onlyHighSchool, someCollege, bachelorsOrMore;
 
-    public EducationInfo(double noHighSchool, double onlyHighSchool, double someCollege, double bachelorsOrMore){
+    public EducationInfo(double noHighSchool, double onlyHighSchool, double someCollege, double bachelorsOrMore) {
         this.noHighSchool = noHighSchool;
         this.onlyHighSchool = onlyHighSchool;
         this.someCollege = someCollege;
         this.bachelorsOrMore = bachelorsOrMore;
     }
-}
+
+    public double getNoHighSchool() {
+        return noHighSchool;
+    }
+
+    public double getOnlyHighSchool() {
+        return onlyHighSchool;
+    }
+
+    public double getSomeCollege() {
+        return someCollege;
+    }
+
+    public double getBachelorsOrMore() {
+        return bachelorsOrMore;
+    }
+
+   }
+
