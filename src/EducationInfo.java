6ebe33ea2af6e58a26026e2c8d3406cdddb1c92b
Rohan Kale <rkale843@student.fuhsd.org>
2019-03-10 18:34:05 -0700

    Made constructor for EducationInfo

---
+++ src/EducationInfo.java
@@ -0,0 +1,10 @@
+public class EducationInfo {
+    private double noHighSchool,onlyHighSchool,someCollege,bachelorsOrMore;
+
+    public EducationInfo(double noHighSchool, double onlyHighSchool, double someCollege, double bachelorsOrMore){
+        this.noHighSchool = noHighSchool;
+        this.onlyHighSchool = onlyHighSchool;
+        this.someCollege = someCollege;
+        this.bachelorsOrMore = bachelorsOrMore;
+    }
+}
